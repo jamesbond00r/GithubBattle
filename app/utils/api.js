bcc4ec0d742c57d596bbab8e9baea5f3d863c93f
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-06 12:10:52 -0600

    fix API issue

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,15 +1,18 @@
 var axios = require('axios');
 
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile (username) {
-  return axios.get('https://api.github.com/users/' + username)
+  return axios.get('https://api.github.com/users/' + username + params)
     .then(function (user) {
       return user.data;
     });
 }
 
 function getRepos (username) {
-  return axios.get('https://api.github.com/users/' + username + '/repos' + '&per_page=100');
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
 function getStarCount (repos) {
