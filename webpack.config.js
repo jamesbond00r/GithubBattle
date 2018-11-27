a01c659b1450ac1851b43918037950c025d1fdcd
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-11-27 12:01:57 -0600

    Refactor, add arrow functions and polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './app/index.js',
+  entry: ['@babel/polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
@@ -17,7 +17,7 @@ module.exports = {
   devServer:{
       historyApiFallback: true
   },
-  mode: 'development',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
