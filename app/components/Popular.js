7aabfd808627e9c4f2418e24e87418894bc34fff
jamesbond00r <37629428+jamesbond00r@users.noreply.github.com>
2018-12-01 21:17:48 -0600

    Refactor class props

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -54,19 +54,16 @@ SelectLanguage.propTypes = {
 };
 
 class Popular extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
+  state = {
       selectedLanguage: 'All',
       repos: null,
-    };
+    }
 
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
+     
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage)
   }
-  updateLanguage(lang) {
+  updateLanguage = (lang) => {
     this.setState(() => ({
         selectedLanguage: lang,
         repos: null
